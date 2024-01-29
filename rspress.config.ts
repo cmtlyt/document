eed21b469e45392dab89879f6de6e70270b992b9
cmtlyt <cmtlyt@163.com>
2024-01-29 15:41:32 +0800

    fix: 修改baseUrl

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'rspress/config'
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  base: 'document',
+  base: '/document/',
   title: 'CL Document',
   description: 'cmtlyt的编程文档',
   icon: '/rspress-icon.png',
